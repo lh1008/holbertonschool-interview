11a552ffbb3402161b35c12327684ebb29ae0068
lh1008 <paintluis@protonmail.com>
2021-02-14 14:01:44 -0500

    Added find_parent, nodes, perfect_tree, and sort_nodes

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,42 +1,136 @@
 #include "binary_trees.h"
 
 /**
- * heap_insert - entry to function
- * Desc: Function that inserts a value into a Max Binary Heap
- * @root: double pointer to heap_t struct type
- * @value: int value to node inserted
- * Return: pointer to inserted node and NULL if failure
- */
+ * heap_insert - entry point from main
+ * Desc: heap_insert function that inserts value with Max Binary Heap
+ * @root: heap_t type double pointer to root
+ * @value: int type value to insert to new node
+ * Return: a pointer to the new node, or NULL on failure
+*/
+
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *insert_node = NULL;
+	heap_t *new_node;
+	heap_t *parent;
 
-	insert_node = malloc(sizeof(heap_t));
+	if (root == NULL)
+		return (NULL);
 
-	insert_node->n = value;
+	parent = find_parent(*root);
+	new_node = binary_tree_node(parent, value);
 
-	if (*root == 0)
+	if (new_node == NULL)
+		return (NULL);
+
+	if (parent == NULL)
+		*root = new_node;
+	else if (parent->left == NULL)
+		parent->left = new_node;
+	else
+		parent->right = new_node;
+
+	sort_nodes(&new_node);
+
+	return (new_node);
+}
+
+/**
+ * sort_nodes - entry point
+ * Desc: sort_nodes function that sorts a child and parent
+ * @node: heap_t type inserted node
+ * Return: voind function
+*/
+void sort_nodes(heap_t **node)
+{
+	heap_t *current;
+	int n;
+
+	current = *node;
+
+	while (current->parent != NULL)
 	{
-		insert_node  = *root;
-		return (insert_node);
-	} else if ((*root)->n == value)
-	{
-		return (insert_node);
-	} else if ((*root)->n < value)
-	{
-		while ((*root)->n < value)
+		if (current->parent->n < current->n)
 		{
-			insert_node->left = *root;
-			return (insert_node);
+			n = current->parent->n;
+			current->parent->n = current->n;
+			current->n = n;
+			*node = current->parent;
 		}
-	} else if ((*root)->n > value)
-	{
-		while ((*root)->n > value)
-		{
-			insert_node->right = *root;
-			return (insert_node);
-		}
+		current = current->parent;
 	}
-	return (insert_node);
+}
 
+/**
+ * perfect_tree - entry point
+ * Desc: perfect_tree function that validates if the tree is perfect
+ * @root: pointer to the root
+ * Return: 1 for true, otherwhise 0
+*/
+int perfect_tree(const heap_t *root)
+{
+	int left, right;
+
+	if (root == NULL)
+		return (0);
+
+	left = nodes(root->left);
+	right = nodes(root->right);
+
+	if (left == right)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * nodes - entry point
+ * Desc: nodes function that counts the left and right nodes
+ * @root: double pointer to the root
+ * Return: number of nodes
+*/
+int nodes(heap_t *root)
+{
+	int left;
+
+	if (root == NULL)
+		return (0);
+
+	if (root)
+		left = 1;
+
+	left += nodes(root->left);
+	left += nodes(root->right);
+
+	return (left);
+}
+
+/**
+ * find_parent - entry point
+ * Desc: find_parent to find parent to insert child
+ * @root: heap_t type double pointer
+ * Return: Pointer to the parent
+*/
+heap_t *find_parent(heap_t *root)
+{
+	heap_t *parent;
+	int left, right, left_l, right_l;
+
+	if (root == NULL)
+		return (NULL);
+
+	parent = root;
+	left = nodes(parent->left);
+	right = nodes(parent->right);
+	left_l = perfect_tree(parent->left);
+	right_l = perfect_tree(parent->right);
+
+	if (left < 1 || right < 1)
+		return (parent);
+
+	if (left_l < 1 || (left_l && right_l && left == right))
+		return (find_parent(parent->left));
+	else if (right_l < 1 || (left_l && right_l && left > right))
+		return (find_parent(parent->right));
+
+	return (parent);
 }
