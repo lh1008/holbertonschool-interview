2985bb5a678a0509c11993d4f49e7a9795b7fe0d
lh1008 <paintluis@protonmail.com>
2021-03-11 16:47:00 -0500

    Finished task

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,19 +3,35 @@
 
 import sys
 
+status_code = {"200": 0, "301": 0,"400": 0, "401": 0,
+               "403": 0, "404": 0, "405": 0, "500": 0}
+output = 0
+
 
 def main():
     """ Method that prints stdin line by line """
-    output = []
-    for line in sys.stdin:
-        print(line.rstrip())
-        words = line.split()
-        output.append(words[-1])
-        if len(output) == 10:
-            output = list(map(int, output))
-            total = sum(output)
-            print('File size: {}'.format(total))
+    print("File size: {}".format(output))
+    for status in sorted(status_code.keys()):
+        if status_code[status]:
+            print("{}: {}".format(status, status_code[status]))
 
 
 if __name__ == '__main__':
+    count = 0
+    try:
+        for line in sys.stdin:
+            try:
+                words = line.split()
+                output += int(words[-1])
+                if words[-2] in status_code:
+                    status_code[words[-2]] += 1
+            except:
+                pass
+            if count == 9:
+                main()
+                count = -1
+            count += 1
+    except KeyboardInterrupt:
+        main()
+        raise
     main()
--- 0x06-log_parsing/generator.py
+++ 0x06-log_parsing/generator.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-import random
-import sys
-from time import sleep
-import datetime
+""" Module that reads stdin line """
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+import sys
+
+
+def main():
+    """ Method that prints stdin line by line """
+    output = []
+    for line in sys.stdin:
+        print(line.rstrip())
+        words = line.split()
+        output.append(words[-1])
+        if len(output) == 10:
+            output = list(map(int, output))
+            total = sum(output)
+            print('File size: {}'.format(total))
+
+
+if __name__ == '__main__':
+    main()
