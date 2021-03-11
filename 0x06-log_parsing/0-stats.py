1cc93f99c4e738f8b35a218177142508b6a19eeb
lh1008 <paintluis@protonmail.com>
2021-03-11 16:25:09 -0500

    Print File size: <total size>

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """ Module that reads stdin line """
 
-import fileinput
 import sys
 
 
 def main():
     """ Method that prints stdin line by line """
     output = []
-    x = 0
     for line in sys.stdin:
         print(line.rstrip())
         words = line.split()
         output.append(words[-1])
         if len(output) == 10:
-            print(output)
+            output = list(map(int, output))
+            total = sum(output)
+            print('File size: {}'.format(total))
 
 
 if __name__ == '__main__':
