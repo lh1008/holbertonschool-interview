761c81a19b401868e5d8c18b5f79e874ba3c8c29
lh1008 <paintluis@protonmail.com>
2021-04-08 18:01:57 -0500

    Module of base 2 numbers

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -10,15 +10,17 @@
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t i, sum = 0;
+	size_t i, sum_2 = 0, sum_4 = 0;
 	(void)direction;
 
 	for (i = 0; i < size; ++i)
 	{
 		printf("%d\n", line[i]);
-		if ((line[i] % 2) == 0)
-			sum += line[i];
+		if (line[i] == 2 && (line[i] % 2) == 0)
+			sum_2 += line[i];
+		else if (line[i] == 4 && ((line[i] % 4) == 0))
+			sum_4 += line[i];
 	}
-	printf("%ld\n", sum);
-	return (sum);
+	printf("%ld\n%ld\n", sum_2, sum_4);
+	return (sum_4);
 }
