e693ef211961aceb454bd748dce6eb9425635623
lh1008 <paintluis@protonmail.com>
2021-04-26 18:29:41 -0500

    Removed print sys.argv when number

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -6,7 +6,6 @@ def main():
     """ Method that solves the N queen problem """
     if len(sys.argv) == 2:
         if sys.argv[1].isdigit():
-            print(sys.argv[1])
             if int(sys.argv[1]) < 4:
                 print('N must be at least 4 number')
         else:
