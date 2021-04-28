bb27197e552549775411dd55a8772e474b0eb831
lh1008 <paintluis@protonmail.com>
2021-04-28 18:36:27 -0500

    Update arguments

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -62,14 +62,14 @@ def main():
         print('Usage: nqueens N')
         sys.exit(1)
 
-    if int(sys.argv[1]) < 4:
-            print('N must be at least 4 number')
-            sys.exit(1)
-
     try:
         int(sys.argv[1])
     except BaseException:
         print('N must be a number')
+        sys.exit(1)
+
+    if int(sys.argv[1]) < 4:
+        print('N must be at least 4')
         sys.exit(1)
 
     nqueens(int(sys.argv[1]))
