b622389b389925fa6e187a7e8746794fcff6607a
lh1008 <paintluis@protonmail.com>
2021-05-06 20:19:10 -0500

    Fixed betty for sorted array to avl function

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -56,7 +56,7 @@ avl_t *sort_data(int *array, size_t start, size_t end, avl_t *parent)
  * sorted_array_to_avl - entry to function sorted array to avl
  * Desc: sorted_array_to_avl function that builds an AVL tree from an array
  * @array: int type pointer to array of numbers
- * @size_t: size_t type size of the array
+ * @size: size_t type size of the array
  * Return: pointer to the root node of the created AVL tree
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
