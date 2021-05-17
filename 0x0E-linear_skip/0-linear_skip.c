d897383d15882a4fac1ef95076a24402f952034a
lh1008 <paintluis@protonmail.com>
2021-05-17 17:00:09 -0500

    Searching for what I'm missing in requirements

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -69,6 +69,9 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	if (list == NULL)
 		return (NULL);
 
+	if (!value)
+		return (NULL);
+
 	while (current != NULL)
 	{
 		next = current->express;
