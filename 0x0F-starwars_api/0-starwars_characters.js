e9267750842a3e1710bfea0266f744723ff37878
lh1008 <paintluis@protonmail.com>
2021-05-17 19:01:35 -0500

    Task 0 working on it, passed arguments and request from API succesful!

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+const https = require('https')
+
+const consoleArg = process.argv.slice(2);
+
+const options = {
+  hostname: 'swapi-api.hbtn.io',
+  port: 443,
+  path: '/api/people/' + consoleArg[0] + '/',
+  method: 'GET'
+}
+
+
+console.log('arg: ', consoleArg);
+
+switch (consoleArg[0]) {
+case 'insult':
+    console.log(consoleArg[1], 'smells quite badly.');
+    break;
+case 'compliment':
+    console.log(consoleArg[1], 'is really cool.');
+    break;
+default:
+    console.log('Sorry, that is not something I know how to do.');
+}
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
---
+++ 0x0F-starwars_api/README.md
@@ -0,0 +1,6 @@
+# 0x0F. Star Wars API
+
+### Tasks
+
+_**0. Star Wars Characters**_
+Write a script that prints all characters of a Star Wars movie.
