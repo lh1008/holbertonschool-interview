114f997822be7023de4e5422d044999b29f235f7
lh1008 <paintluis@protonmail.com>
2021-05-18 20:25:58 -0500

    Not finished yet, trying something new

--- 0x0F-starwars_api/starwars_characters.js
+++ 0x0F-starwars_api/starwars_characters.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const https = require('https')
+const https = require('https');
 
-const consoleArg = process.argv[2];
+const consoleArg = process.argv.slice(2);
 
 const options = {
   hostname: 'swapi-api.hbtn.io',
@@ -17,21 +17,14 @@ const req = https.request(options, res => {
   console.log(`statusCode: ${res.statusCode}`)
 
   res.on('data', d => {
-    process.stdout.write(d);
-    const obj = JSON.parse(d, function (key, value) {
-      if (key == "characters") {
-	console.log(value)
-	console.log(JSON.stringify(value));
-      }
-    })
     const characters = JSON.parse(d).characters;
     console.log(characters);
+  });
 
-  })
-})
+});
 
 req.on('error', error => {
   console.error(error)
-})
+});
 
 req.end()
