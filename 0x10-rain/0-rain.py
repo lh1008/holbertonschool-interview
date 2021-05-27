ddb37fcfebec6ae7cd4291119efe02a91978fa33
lh1008 <paintluis@protonmail.com>
2021-05-27 16:49:12 -0500

    First tryout, Task 0 for rain in Python

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Module for rain method that retains rain """
+
+
+def rain(walls):
+    """
+    Method to calculate how many square units of
+    water will be retained after it rains.
+    """
+
+    if (walls == 0):
+        return 0
+
+    res = 0
+    n = range(walls)
+
+    for i in range(1, n-1):
+        left = walls[i]
+
+        for j in range(i):
+            left = max(left, walls[j])
+
+        right = walls[i]
+
+        for j in range(i+1, n):
+            right = max(right, walls[j])
+
+        res = res + (min(left, right) - walls[i])
+
+    return res
+
+
+if __name__ == "__main__":
