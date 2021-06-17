4778d23d3fd363443222df4c31e8c5d0eee2c90b
lh1008 <paintluis@protonmail.com>
2021-06-17 18:12:43 -0500

    Task 0, Count It, parses titles and prints sorted counts of given words

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Module that queries the Reddit API """
+
+
+import requests
+
+
+def count_words(subreddit, word_list, after='', word_count={}):
+    """
+    Method that parses the title of all hot articles,
+    and prints a sorted count of given keywords.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    h = {'User-Agent': 'holberton_school'}
+    payload = {'limit': '100', 'after': after}
+    r = requests.get(url, headers=h, params=payload, allow_redirects=False)
+    if r.status_code == 200:
+        data = r.json().get('data')
+        after = data.get('after')
+        children = data.get('children')
+        for child in children:
+            title = child.get('data').get('title')
+            for word in word_list:
+                ocurrences = title.lower().split().count(word.lower())
+                if ocurrences > 0:
+                    if word in word_count:
+                        word_count[word] += ocurrences
+                    else:
+                        word_count[word] = ocurrences
+
+        if after is not None:
+            return count_words(subreddit, word_list, after, word_count)
+        else:
+            if not len(word_count) > 0:
+                return
+            datas = sorted(word_count.items(),
+                           key=lambda key_value: (-key_value[1], key_value[0]))
+            for key, value in datas:
+                print('{}: {}'.format(key.lower(), value))
+    else:
+        return
