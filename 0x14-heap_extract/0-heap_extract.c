6ec30a9242cb06878b58600c82da6af292878457
lh1008 <paintluis@protonmail.com>
2021-06-21 15:48:11 -0500

    Added heap extract function

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - entry to heap extract
+ * Desc: heap_extract function that extracts
+ * the root node of a Max Binary Heap
+ * @root: double pointer to the root node of the heap
+ * Return: value stored in the root node, 0 if it fails
+ * https://algorithmtutor.com/Data-Structures/Tree/Binary-Heaps/
+ * https://www.programiz.com/dsa/heap-data-structure
+ */
+int heap_extract(heap_t **root)
+{
+	
+}
