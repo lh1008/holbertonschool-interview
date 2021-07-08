0d03ebb59bc4d627e881bdc6ea8133cea13a4315
lh1008 <paintluis@protonmail.com>
2021-07-08 16:46:24 -0500

    Task 0 done, rotate 90 degrees

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module that rotates a 2D matrix """
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Method that rotates a 2D matrix 90 degrees clockwise
+    Args:
+        matrix (list[[list]]): a matrix
+    """
+    temp_matrix = []
+    column = len(matrix)-1
+    for column in range(len(matrix)):
+        temp = []
+        for row in range(len(matrix)-1,-1,-1):
+            temp.append(matrix[row][column])
+        temp_matrix.append(temp)
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            matrix[i][j] = temp_matrix[i][j]
