661dae807cd9cf5da0865ee24cb17ce34bff93b0
lh1008 <paintluis@protonmail.com>
2021-07-13 14:54:53 -0500

    Added function file to find loop project

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - entry to find listint loop
+ * Desc: find_listint_loop function that finds the
+ * loop in a linked list.
+ * @head: pointer to node structure
+ * Return: the address of the node where the loop
+ * starts, or NULL if there's no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+}
