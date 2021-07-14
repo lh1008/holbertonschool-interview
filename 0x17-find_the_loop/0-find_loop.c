fa69087fd1c22a0426646ec9e34809d722425a6d
lh1008 <paintluis@protonmail.com>
2021-07-14 11:02:43 -0500

    Find a loop function done with compiled executable

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -10,4 +10,19 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *slow, *fast;
+
+	if (head == NULL)
+		return (NULL);
+
+	slow = fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (slow);
+	}
+	return (NULL);
 }
