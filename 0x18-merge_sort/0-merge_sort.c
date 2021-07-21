99a4c822ccffd046045954a3849cf0d91eecaf85
lh1008 <paintluis@protonmail.com>
2021-07-21 15:01:45 -0500

    Added 0-merge_sort.c file standar structure

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * merge_sort - entry to merge sort
+ * Desc: merge_sort function that sorts an array
+ * of integers in ascending order using the Merge Sort algorithm.
+ * @array: point to int array
+ * @size: size_t array
+ * Return: sort ascending order algorithm
+ */
+void merge_sort(int *array, size_t size)
+{
+}
