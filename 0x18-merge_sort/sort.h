cac669531cf01f184aced0801a9e955793f54e31
lh1008 <paintluis@protonmail.com>
2021-07-21 15:50:35 -0500

    Finished mergin sort algorithm

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -1,6 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
 
