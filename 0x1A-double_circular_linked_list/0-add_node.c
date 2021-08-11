1551d14025aea8636490b25f7426cf4eec192d53
lh1008 <paintluis@protonmail.com>
2021-08-11 14:38:18 -0500

    Finished double_circular_linked_list function

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,73 @@
+#include "list.h"
+
+/**
+ * add_node_end - entry to add node end
+ * Desc: add_node_end function that adds a new node
+ * to the end of a double circular linked list
+ * @list: double pointer to List struct
+ * @str: char type string to copy to new node
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new_node, *last;
+
+	if (*list == NULL)
+	{
+		new_node = malloc(sizeof(List));
+		new_node->str = str;
+		new_node->next = new_node->prev = new_node;
+		*list = new_node;
+		return (new_node);
+	}
+
+	last = (*list)->prev;
+
+	new_node = malloc(sizeof(List));
+	new_node->str = str;
+
+	new_node->next = *list;
+	(*list)->prev = new_node;
+	new_node->prev = last;
+	last->next = new_node;
+	return (new_node);
+
+}
+
+/**
+ * add_node_begin - entry to add node begin
+ * Desc: add_node_begin function that adds a new node
+ * to the beginning of a double circular linked list
+ * @list: double pointer to List struct
+ * @str: char type string to modify
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *last, *new_node;
+
+	if (*list == NULL)
+	{
+		new_node = malloc(sizeof(List));
+		new_node->str = str;
+		new_node->next = new_node->prev = new_node;
+		*list = new_node;
+		return (new_node);
+	}
+
+	last = malloc(sizeof(List));
+
+	last = (*list)->prev;
+
+	new_node = malloc(sizeof(List));
+	new_node->str = str;
+
+	new_node->next = *list;
+	new_node->prev = last;
+
+	last->next = (*list)->prev = new_node;
+
+	*list = new_node;
+
+	return (new_node);
+}
--- 0x1A-double_circular_linked_list/list.h
+++ 0x1A-double_circular_linked_list/list.h
@@ -1,6 +1,9 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct List - doubly linked list
  * @str: string - (malloc'ed string)
