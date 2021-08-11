83511c5761887c16ccc788ffa7cb81867846044a
lh1008 <paintluis@protonmail.com>
2021-08-11 14:01:07 -0500

    Added list.h header with prototypes ready

---
+++ 0x1A-double_circular_linked_list/list.h
@@ -0,0 +1,26 @@
+#ifndef LIST_H
+#define LIST_H
+
+/**
+ * struct List - doubly linked list
+ * @str: string - (malloc'ed string)
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct List
+{
+  char *str;
+  struct List *prev;
+  struct List *next;
+} List;
+
+/* New node function of a double circular linked list */
+List *add_node_end(List **list, char *str);
+
+/* New node to the beginning of a double circular linked list */
+List *add_node_begin(List **list, char *str);
+
+#endif /* LIST_H */
