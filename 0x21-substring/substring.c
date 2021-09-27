a757cde1dae894646a14e377397f74aa74c50ed5
lh1008 <paintluis@protonmail.com>
2021-09-27 16:48:17 -0500

    Added code to function substring.c

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -1,4 +1,4 @@
-#include "substring.c"
+#include "substring.h"
 
 /**
  * find_substring - entry to function
@@ -14,13 +14,40 @@
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-	int size_word, size_words, index_store = 0;
+	int size_word, s_len, j, i = 0, k;
+	int *elements, *found;
 
-	size_word = strlen(*words);
+	if (!s)
+		return (NULL);
 
-	size_words = size_word * nb_words;
-
-	if (size_words > strlen(s))
-		return (index_store);
-
+	size_word = strlen(words[0]);
+	*n = 0;
+	s_len = strlen(s);
+	found = malloc(nb_words * sizeof(int));
+	elements = malloc(s_len * sizeof(int));
+	while (i <= s_len - nb_words * size_word)
+	{
+		for (j = 0; j < nb_words; j++)
+			found[j] = 0;
+		for (j = 0; j < nb_words; j++)
+		{
+			for (k = 0; k < nb_words; k++)
+			{
+				if (found[k] == 0 &&
+					strncmp(s + i + j * size_word,
+						words[k], size_word) == 0)
+				{
+					found[k] = 1;
+					break;
+				}
+			}
+			if (k == nb_words)
+				break;
+		}
+		if (j == nb_words)
+			elements[(*n)++] = i;
+		i++;
+	}
+	free(found);
+	return (elements);
 }
--- 0x21-substring/substring.h
+++ 0x21-substring/substring.h
@@ -1,6 +1,10 @@
 #ifndef SUBSTRING_H
 #define SUBSTRING_H
 
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
 
 #endif /* SUBTRING_H */
